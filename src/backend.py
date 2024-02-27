3709f661cd79c1b806a94ad0444a040e13ab17ae
Otacilio Santos <otaciliopedro.dossantos@pepsico.com>
2024-02-27 09:30:13 +0100

    feature: challenge on postgres

--- src/backend.py
+++ src/backend.py
@@ -1,18 +1,33 @@
+from sqlmodel import create_engine, Session, text, SQLModel, Field
+from dotenv import load_dotenv
 import pandas as pd
-import sqlite3
+import os
+
+load_dotenv()
 
 class ChallengeDB():
     invalid_query_exception = pd.errors.DatabaseError
 
     def __init__(self):
-        self.conn = sqlite3.connect('data/chinook.db')
+        db_user = os.getenv('POSTGRES_CHALLENGER_USER')
+        db_password = os.getenv('POSTGRES_CHALLENGER_PASSWORD')
+        db_host = os.getenv('POSTGRES_HOST')
+        conn_string = f"cockroachdb://{db_user}:{db_password}@{db_host}:26257/challenge_db"
+        self._engine = create_engine(conn_string)
 
     def retrive_results(self, query):
-        return pd.read_sql_query(query, self.conn)
+        conn = self._engine.connect()
+        df = pd.read_sql_query(query, conn)
+        conn.close()
+        return df
 
     def retrive_solution(self, challenge_number):
+        conn = self._engine.connect()
         query = f"SELECT * FROM solution_{challenge_number}"
-        return pd.read_sql_query(query, self.conn)
+        df = pd.read_sql_query(query, conn)
+        conn.close()
+        return df
+
 
 class BackendDB():
 
