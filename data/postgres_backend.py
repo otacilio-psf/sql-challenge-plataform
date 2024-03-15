98eb7b24677586ebd1f77c193b451c797435237c
Otacilio Santos <otaciliopedro.dossantos@pepsico.com>
2024-03-15 13:00:12 +0100

    change: improve code for data folder

--- data/postgres_backend.py
+++ data/postgres_backend.py
@@ -1,4 +1,4 @@
-from sqlmodel import create_engine, SQLModel
+from sqlmodel import create_engine, SQLModel, Session, text
 from dotenv import load_dotenv
 import os
 import sys
@@ -9,12 +9,19 @@ load_dotenv()
 
 db_user_password = os.getenv('POSTGRES_ADM_USER_PASSWORD')
 db_host = os.getenv('POSTGRES_HOST')
-conn_string = f"cockroachdb://{db_user_password}@{db_host}:26257/backend_db"
 
-engine = create_engine(conn_string)
+def create_database():
+    conn_string = f"cockroachdb://{db_user_password}@{db_host}:26257/defaultdb"
+    engine = create_engine(conn_string)
+    with Session(engine) as session:
+        session.exec(text("CREATE DATABASE backend_db"))
+        session.commit()
 
 def main():
+    conn_string = f"cockroachdb://{db_user_password}@{db_host}:26257/backend_db"
+    engine = create_engine(conn_string)
     SQLModel.metadata.create_all(engine)
 
 if __name__ == '__main__':
+    create_database()
     main()
--- data/postgres_challenge_data.py
+++ data/postgres_challenge_data.py
@@ -1,4 +1,4 @@
-from sqlmodel import create_engine
+from sqlmodel import create_engine, Session, text
 from dotenv import load_dotenv
 import pandas as pd
 import os
@@ -7,20 +7,29 @@ load_dotenv()
 
 db_user_password = os.getenv('POSTGRES_ADM_USER_PASSWORD')
 db_host = os.getenv('POSTGRES_HOST')
-conn_string = f"cockroachdb://{db_user_password}@{db_host}:26257/challenge_db"
 
-engine = create_engine(conn_string)
+
+def create_database():
+    conn_string = f"cockroachdb://{db_user_password}@{db_host}:26257/defaultdb"
+    engine = create_engine(conn_string)
+    with Session(engine) as session:
+        session.exec(text("CREATE DATABASE challenge_db"))
+        session.commit()
 
 def load_challenge_data():
-    files = os.listdir("data/challange_datasets")
+    conn_string = f"cockroachdb://{db_user_password}@{db_host}:26257/challenge_db"
+    engine = create_engine(conn_string)
+    files = os.listdir("data/challenge_datasets")
     for f in files:
+        print("Loading dataset:", f)
         table_name = f.split(".")[0]
-        pd.read_parquet(f"data/challange_datasets/{f}").to_sql(table_name, engine)
+        pd.read_parquet(f"data/challenge_datasets/{f}").to_sql(table_name, engine)
 
 
 def load_solution():
     pass
 
 if __name__ == '__main__':
+    create_database()
     load_challenge_data()
     load_solution()
